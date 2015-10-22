12fed27c421b599909bacca02d1343f79546513a
Fauz O. Ecum <fauz.ecum@foecum.com>
2015-10-22 10:28:20 +0200

    Problem 11

--- Problem 12/main.cpp
+++ Problem 12/main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+int getDivisorCount(long int num){
+    int count = 0;
+    for(long int i = 1; i <= sqrt(num); i++){
+        if(num % i == 0){
+            count += 2;
+        }
+    }
+    return count;
+}
+
+int getTriangleNumber(){
+    long int nextNumber = 1, incr = 2;
+    int divisors = 0;
+    while(divisors <= 500){
+        nextNumber += incr++;
+        divisors = getDivisorCount(nextNumber);
+    }
+
+    cout << "The first triangle number with more than 500 divisors is " << nextNumber << endl;
+}
+int main(){
+    getTriangleNumber();
     return 0;
 }
